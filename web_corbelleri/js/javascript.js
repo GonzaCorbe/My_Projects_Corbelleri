3e5a565ef4500175bc87099a73cc40e2ec48f1b3
GonzaCorbe <147212370+GonzaCorbe@users.noreply.github.com>
2024-06-14 01:29:39 -0300

    Add files via upload

---
+++ web_corbelleri/js/javascript.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('myForm');
+
+    form.addEventListener('submit', function(event) {
+        var nombre = document.getElementById('nombre').value.trim();
+        var apellido = document.getElementById('apellido').value.trim();
+        var email = document.getElementById('email').value.trim();
+        var mensaje = document.getElementById('mensaje').value.trim();
+
+        if (!nombre || !apellido || !email || !mensaje) {
+            event.preventDefault();
+            alert('Por favor, completa todos los campos.');
+        }
+
+        if (email.indexOf('@') === -1) {
+            event.preventDefault();
+            alert('El campo de correo electrónico debe contener "@".');
+        }
+    });
+});
